79a4cf9402f07b40ec61c9b93e3c4ef4bb652d69
H-strangeone <hiteshgitman@gmail.com>
2024-01-11 23:25:44 -0800

    x

--- fahrenheight to celcius.cpp
+++ fahrenheight to celcius.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"input n"<<endl;
+    cin>>n;
+    cout<<"\n";
+    while (n>=0)
+    {
+        int fah=(n*9/5)+32;
+        cout<<"when the temp in c is= "<<n<<endl;
+        cout<<"the temp in fahrenheit is= "<<fah<<endl;
+        cout<<"\n";
+        n--;
+    }
+    return 0;
 }
